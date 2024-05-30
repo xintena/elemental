8ae07a742a4f41497ed3eb58f668cf8675e035b0
Andrea Mazzotti <andrea.mazzotti@suse.com>
2024-05-30 17:35:10 +0200

    Make the refresh script a bit more maintainable
    Signed-off-by: Andrea Mazzotti <andrea.mazzotti@suse.com>

--- refresh_channels.sh
+++ refresh_channels.sh
@@ -2,85 +2,108 @@
 
 set -e
 
+# Prefixes the ManagedOSVersion name with flavor value, if any
+function format_managed_os_version_name() {
+    local flavor=$1
+    local tag=$2
+    if [ -z "$flavor" ]; then
+        echo "v${tag}"
+    else
+        echo "${flavor}-v${tag}"
+    fi
+}
+
+# Prints one OS JSON array entry
+function append_os_entry() {
+    local file=$1
+    local os_version_name=$2
+    local version=$3
+    local image_uri=$4
+    local display_name=$5
+    cat >> "$file" << EOF
+    {
+        "metadata": {
+            "name": "$os_version_name"
+        },
+        "spec": {
+            "version": "v$version",
+            "type": "container",
+            "metadata": {
+                "upgradeImage": "$image_uri",
+                "displayName": "$display_name"
+            }
+        }
+    },
+EOF
+}
+
+# Prints one ISO JSON array entry
+function append_iso_entry() {
+    local file=$1
+    local os_version_name=$2
+    local version=$3
+    local image_uri=$4
+    local display_name=$5
+    cat >> "$file" << EOF
+    {
+        "metadata": {
+            "name": "$os_version_name"
+        },
+        "spec": {
+            "version": "v$version",
+            "type": "iso",
+            "metadata": {
+                "uri": "$image_uri",
+                "displayName": "$display_name"
+            }
+        }
+    },
+EOF
+}
+
+# Return the 'org.opencontainers.image.version' label value
+#
+# This value should always point to the latest build version tag.
+# Ex. '2.0.2-4.2.102' instead of '2.0.2'
+function fetch_build_tag() {
+    local repo=$1
+    local tag=$2
+
+    local build_tag=($(skopeo inspect docker://$repo:$tag | jq '.Labels["org.opencontainers.image.version"]' | sed 's/"//g'))
+    echo "$build_tag"
+}
+
 watches=$(yq e -o=j -I=0 '.watches[]' config.yaml)
 
 # Loop through all watches
 while IFS=\= read watch; do
     # Parse one entry
     flavor=$(echo "$watch" | yq e '.flavor')
-    fileName=$(echo "$watch" | yq e '.fileName')
-    displayName=$(echo "$watch" | yq e '.displayName')
-    osRepo=$(echo "$watch" | yq e '.osRepo')
-    isoRepo=$(echo "$watch" | yq e '.isoRepo')
+    file_name=$(echo "$watch" | yq e '.fileName')
+    display_name=$(echo "$watch" | yq e '.displayName')
+    os_repo=$(echo "$watch" | yq e '.osRepo')
+    iso_repo=$(echo "$watch" | yq e '.isoRepo')
 
-    # Fetch the OS Image tags
-    osTags=($(skopeo list-tags docker://$osRepo | jq '.Tags[]' | grep -v '.att\|.sig\|latest' | sed 's/"//g'))
-
-    # Start writing the channel file
-    file="channels/$fileName.json"
-    echo "Creating $fileName"
+    # Start writing the channel file by opening a JSON array
+    file="channels/$file_name.json"
+    echo "Creating $file_name"
     echo "[" > $file
-    for tag in "${osTags[@]}"; do
-        # Determine ManagedOSVersion name
-        if [ -z $flavor ]; then
-            name="v${tag}"
-        else
-            name="${flavor}-v${tag}"
-        fi
-        # Fetch image tag
-        #
-        # Note that we use the 'org.opencontainers.image.version'
-        # so that we can always point to the latest build version tag.
-        # Ex. '2.0.2-4.2.102' instead of '2.0.2'
-        imageTag=($(skopeo inspect docker://$osRepo:$tag | jq '.Labels["org.opencontainers.image.version"]' | sed 's/"//g'))
-        cat << EOF >> $file
-    {
-        "metadata": {
-            "name": "$name"
-        },
-        "spec": {
-            "version": "v$tag",
-            "type": "container",
-            "metadata": {
-                "upgradeImage": "$osRepo:$imageTag",
-                "displayName": "$displayName OS"
-            }
-        }
-    },
-EOF
+
+    # Process OS container tags
+    os_tags=($(skopeo list-tags docker://$os_repo | jq '.Tags[]' | grep -v '.att\|.sig\|latest' | sed 's/"//g'))
+    for tag in "${os_tags[@]}"; do
+        managed_os_version_name=$(format_managed_os_version_name "$flavor" "$tag")
+        build_tag=$(fetch_build_tag "$os_repo" "$tag")
+        append_os_entry "$file" "$managed_os_version_name" "$tag" "$os_repo:$build_tag" "$display_name OS"
     done
 
-    ## Fetch the ISO Image tags (if applicable)
-    if [ $isoRepo != "N/A" ]; then
-        isoTags=($(skopeo list-tags docker://$isoRepo | jq '.Tags[]' | grep -v '.att\|.sig\|latest' | sed 's/"//g'))
-        for tag in "${isoTags[@]}"; do
-            # Determine ManagedOSVersion name
-            if [ -z $flavor ]; then
-                name="v${tag}"
-            else
-                name="${flavor}-v${tag}"
-            fi
-            # Fetch image tag
-            #
-            # Note that we use the 'org.opencontainers.image.version'
-            # so that we can always point to the latest build version tag.
-            # Ex. '2.0.2-4.2.102' instead of '2.0.2'
-            imageTag=($(skopeo inspect docker://$isoRepo:$tag | jq '.Labels["org.opencontainers.image.version"]' | sed 's/"//g'))
-            cat << EOF >> $file
-    {
-        "metadata": {
-            "name": "$name"
-        },
-        "spec": {
-            "version": "v$tag",
-            "type": "iso",
-            "metadata": {
-                "uri": "$isoRepo:$imageTag",
-                "displayName": "$displayName ISO"
-            }
-        }
-    },
-EOF
+    # Process ISO container tags (if applicable)
+    if [ "$iso_repo" != "N/A" ]; then
+        iso_tags=($(skopeo list-tags docker://$iso_repo | jq '.Tags[]' | grep -v '.att\|.sig\|latest' | sed 's/"//g'))
+        for tag in "${iso_tags[@]}"; do
+            managed_os_version_name=$(format_managed_os_version_name "$flavor" "$tag")
+            build_tag=$(fetch_build_tag "$iso_repo" "$tag")
+            append_iso_entry "$file" "$managed_os_version_name" "$tag" "$iso_repo:$build_tag" "$display_name ISO"
         done
     fi
 
@@ -92,6 +115,6 @@ EOF
 
     # Validate the JSON file
     cat $file | jq empty
-done <<EOF
+done <<END
 $watches
-EOF
+END
